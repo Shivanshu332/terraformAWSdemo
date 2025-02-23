edc77ae3ef1cf4fbe2694e51c4d58055281036ca
Shivanshu Dubey <shhivanshu.cdubey332@gmail.com>
2025-02-24 03:00:22 +0530

    correcting syntax

--- environment/dev/module.tf
+++ environment/dev/module.tf
@@ -1,14 +1,24 @@
-module "vpc" {
-  source                = "../../ec2_instance"
-  vpc_name              = var.vpc_name
-  public_subnet_name    = var.public_subnet_name
-  private_subnet_name   = var.private_subnet_name
-  internet_gateway_name = var.internet_gateway_name
-  security_group_name   = var.security_group_name
-  ami_name              = var.ami_name
-  virtualization_type   = var.virtualization_type
-  instance_type_public  = var.instance_type_public
-  instance_type_private = var.instance_type_private
-  instance_name_public  = var.instance_name_public
-  instance_name_private = var.instance_name_private
+module  vpc  {
+  source                      = "../../ec2_instance"
+  vpc_name                    = var.vpc_name
+  public_subnet_name          = var.public_subnet_name
+  private_subnet_name         = var.private_subnet_name
+  internet_gateway_name       = var.internet_gateway_name
+  vpc_required                = var.vpc_required             
+  private_subnet_required     = var.private_subnet_required  
+  public_subnet_required      = var.public_subnet_required   
+  internet_gateway_required   = var.internet_gateway_required
+  security_group_name         = var.security_group_name
+  ami_name                    = var.ami_name
+  virtualization_type         = var.virtualization_type
+  instance_type_public        = var.instance_type_public
+  instance_type_private       = var.instance_type_private
+  instance_name_public        = var.instance_name_public
+  instance_name_private       = var.instance_name_private
+  desired_capacity_public     = var.desired_capacity_public
+  max_size_public             = var.max_size_public        
+  min_size_public             = var.min_size_public        
+  desired_capacity_private    = var.desired_capacity_private
+  min_size_private            = var.min_size_private       
+  max_size_private            = var.max_size_private
 }
