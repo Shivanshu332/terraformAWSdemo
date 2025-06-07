6916b6998c10708e9bd624b1b828cb2f7dc71e31
Shivanshu Dubey <shhivanshu.cdubey332@gmail.com>
2025-06-08 02:52:11 +0530

    correcting syntax

--- loadbalancer/loadbalancer.tf
+++ loadbalancer/loadbalancer.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_target_group" "tg" {
-    name     = "web-app_tg"
+    name     = "web-app-tg"
     port     = 80
     protocol = "HTTP"
     vpc_id   = var.vpc_id
