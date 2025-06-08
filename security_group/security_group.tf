c920e7fee7e4f74e5c2cccbf3f3955b0198a3d5c
Shivanshu Dubey <shhivanshu.cdubey332@gmail.com>
2025-06-08 13:10:40 +0530

    security group correction

--- security_group/security_group.tf
+++ security_group/security_group.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "web_instance_SG" {
     ingress {
         from_port   = 80
         to_port     = 80
-        protocol    = "http"
+        protocol    = "tcp"
         security_groups = [ aws_security_group.web_loadbalancer_SG.id ]
     }
 
