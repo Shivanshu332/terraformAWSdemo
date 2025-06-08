36b982ce36ce4d2c27c988b9f3f31c48c3bb4abf
Shivanshu Dubey <shhivanshu.cdubey332@gmail.com>
2025-06-08 11:45:05 +0530

    Correcting syntax

--- environment/dev/variable.tf
+++ environment/dev/variable.tf
@@ -13,13 +13,8 @@ variable "environment"                  { type = string }
 //asg variables
 variable "ami_name"                     { type = string }
 variable "virtualization_type"          { type = string }
-variable "instance_type_public"         { type = string }
 variable "instance_type_private"        { type = string }
-variable "instance_name_public"         { type = string }
 variable "instance_name_private"        { type = string }
-variable "desired_capacity_public"      { type = number }
-variable "max_size_public"              { type = number }
-variable "min_size_public"              { type = number }
 variable "desired_capacity_private"     { type = number }
 variable "min_size_private"             { type = number }
 variable "max_size_private"             { type = number }
